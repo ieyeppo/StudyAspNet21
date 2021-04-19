62cea5d7a8c11ffd8dcebe4eb7e30c25ab69d34d
ieyeppo <ieyeppo@gmail.com>
2021-04-19 17:26:40 +0900

    커밋

---
+++ MvcSolution/MvcWebLab.Database/dbo/Tables/Tasks.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Tasks]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(150) NOT NULL, 
+    [isDone] BIT NULL, 
+    [CreationDate] DATETIME NULL DEFAULT GetDate()
+)
---
+++ MvcSolution/TodoListWebApp/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListWebApp.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public bool isDone { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
