de776efc2a0735d163c34d092e88f68e470925b1
ieyeppo <ieyeppo@gmail.com>
2021-04-20 17:27:44 +0900

    템플릿 이요한 홈페이지 만들기 커밋

---
+++ MvcSolution/RestApiTestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestApiTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
